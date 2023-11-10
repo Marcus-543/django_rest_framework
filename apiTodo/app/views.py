c3eff4b52bd7c0ac8271f5ccc199f4141af20d37
Marcus-Linhares <marcusviniciuslinhares543@gmail.com>
2023-11-10 18:40:38 -0300

    classe baseViews APIViews addeds

--- apiTodo/app/views.py
+++ apiTodo/app/views.py
@@ -3,37 +3,44 @@ from app.models import Todo
 from app.serializers import TodoSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound
 
-@api_view(['GET', 'POST'])
-def todo_list(request):
-    if request.method == 'GET':
+class TodoListAndCreate(APIView):
+    def get(self, request):
         todo = Todo.objects.all()
         serializer = TodoSerializer(todo, many = True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = TodoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def todo_detail_change_and_delete(request, pk):
-    try:
-        todo = Todo.objects.get(pk=pk)
-    except Todo.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class TodoDetailChangeAndDelete(APIView):
+    def getObject(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            raise NotFound()
 
-    if request.method == 'GET':
+    def get(self, request, pk):
+        todo = self.getObject(pk)
         serializer = TodoSerializer(todo)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk):
+        todo = self.getObject(pk)
         serializer = TodoSerializer(todo, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+    
+    def delete(self, request, pk):
+        todo = self.getObject(pk)
         todo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)            
+        return Response(status=status.HTTP_204_NO_CONTENT)   
         
